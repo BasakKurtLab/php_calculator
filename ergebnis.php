79b72672375bc0c4e333ce0946913261098b0425
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-10-30 04:16:33 +0200

    initial commit

---
+++ ergebnis.php
@@ -0,0 +1,56 @@
+<?php
+                if(isset($_POST["calc"]))
+                {
+                    $first_nr=$_POST["first_nr"];
+                    $second_nr=$_POST["second_nr"];
+                    $selection=$_POST["selection"];
+                    $result= 0;
+                
+               
+
+                    switch ($selection){
+                        
+                        case '+':
+                            $result = $first_nr + $second_nr;
+                        break;
+                        case '-':
+                            $result = $first_nr - $second_nr;
+                        break;
+                        case '*':
+                            $result = $first_nr * $second_nr;
+                        break;
+                        case '/':
+
+                            if($second_nr== 0){
+                                header('location:index.php?w_msg= Zweite Zahl kann nicht 0 sein.');
+                                return;
+                             }
+                             else
+                            $result = $first_nr / $second_nr;
+                            
+                        }
+                        header('location:index.php?r_msg='. $result);
+
+                    /*echo "<h1 class='text-info'>$result</h1>";*/
+
+
+
+
+                    
+                    
+                   
+            }
+            ?>
+                  
+
+
+
+
+
+
+
+   
+
+
+
+
---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>CALCULATOR IN PHP</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    </head>
+    <body>
+      <div class="container" style="margin-top:20px;">
+      <h1 style="text-align:center;">Taschenrechner</h1>
+            <form action="ergebnis.php" method="post">
+              <div class="form-group">
+                <label for="first_nr">Zahl 1</label>
+                <input type="text" class="form-control" name="first_nr">
+              </div>
+              <div class="form-group">
+                <label for="second_nr">Zahl 2</label>
+                <input type="text" class="form-control" name="second_nr">
+              </div>
+              <div class="form-group">
+                <label for="selection">Wahlen Sie bitte</label>
+                <select name="selection" class="form-control">
+                    <option value="+">Addieren</option>
+                    <option value="-">Subtrahieren</option>
+                    <option value="*">Multiplizieren</option>
+                    <option value="/">Dividieren</option>
+                </select>
+              </div>
+              <button type="submit" class="btn btn-danger" name="calc" class="btn btn-default" >Rechnen</button>
+            </form>
+
+            <?php if(isset($_GET['w_msg'])){
+    echo " <h2>Achtung: ".$_GET['w_msg']."</h2>";
+}
+?>
+<?php if(isset($_GET['r_msg'])){
+    echo "<h1>Ergebnis: ".$_GET['r_msg']."</h1>";
+}
+?>
+        </div>
+     
+      
+       
+    </body>
+</html>
