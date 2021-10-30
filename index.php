8e92087e5439d2a1256be28ac4193e2331a0e574
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-10-30 04:23:12 +0200

    commit4

--- index.php
+++ index.php
@@ -8,15 +8,18 @@
     <body>
       <div class="container" style="margin-top:20px;">
       <h1 style="text-align:center;">Taschenrechner</h1>
-            <form action="ergebnis.php" method="post">
+           
+      <form action="ergebnis.php" method="post">
               <div class="form-group">
                 <label for="first_nr">Zahl 1</label>
                 <input type="text" class="form-control" name="first_nr">
               </div>
+             
               <div class="form-group">
                 <label for="second_nr">Zahl 2</label>
                 <input type="text" class="form-control" name="second_nr">
               </div>
+              
               <div class="form-group">
                 <label for="selection">Wahlen Sie bitte</label>
                 <select name="selection" class="form-control">
@@ -26,14 +29,15 @@
                     <option value="/">Dividieren</option>
                 </select>
               </div>
+             
               <button type="submit" class="btn btn-danger" name="calc" class="btn btn-default" >Rechnen</button>
             </form>
 
-            <?php if(isset($_GET['w_msg'])){
+                <?php if(isset($_GET['w_msg'])){
     echo " <h2>Achtung: ".$_GET['w_msg']."</h2>";
 }
 ?>
-<?php if(isset($_GET['r_msg'])){
+                <?php if(isset($_GET['r_msg'])){
     echo "<h1>Ergebnis: ".$_GET['r_msg']."</h1>";
 }
 ?>
